85dc560d9a3ba2aaf19de760a4d1542f8e7f3684
JCPistell <JCPistell@gmail.com>
2015-12-11 07:37:32 -0700

    commented lp model

--- integeroptimization.R
+++ integeroptimization.R
@@ -1,6 +1,7 @@
 library(lpSolve)
 library(dplyr)
 
+#Set up the pieces
 constMatrix <- select(locs, ends_with("Time"))
 constMatrix$build <- 1
 constMatrix <- as.matrix(constMatrix)
@@ -8,6 +9,7 @@ locs.obj <- locs$weightedtimes
 signs <- rep(c("<", "="), c(6, 1))
 rhs <- rep(c(360, 1), c(6, 1))
 
+#Run the model
 optisolve <- lp(direction = "min", 
                 objective.in = locs.obj, 
                 const.mat = constMatrix, 
@@ -16,4 +18,5 @@ optisolve <- lp(direction = "min",
                 transpose.constraints = FALSE,
                 all.bin = TRUE)
 
+#Index the solution
 optimized.loc <- which.max(optisolve$solution)
